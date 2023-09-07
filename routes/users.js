a8120f4bc75cc97671bfd85710bd7a3a4c577609
nursat.bakyt <itsnursat@gmail.com>
2023-09-08 00:13:18 +0600

    first commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+const { login, register, current } = require('../controllers/users');
+var router = express.Router();
+const { auth } = require('./../middleware/auth')
+
+/* /api/user/login */
+router.post('/login', login);
+
+/* /api/user/register */
+router.post('/register', register);
+
+/* /api/user/current */
+router.get('/current', auth, current);
+
+module.exports = router;
